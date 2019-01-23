0b8b1a0a7d5ddf2d1fc742d308703c943084949c
William Andrus <andrus_wj@hotmail.com>
2019-01-23 13:41:15 -0700

    add dukenuke example

---
+++ Containers/dukenukemquotes/dukenukemquotes/Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+
+namespace dukenukemquotes
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var content = JsonConvert.DeserializeObject<Content>(File.ReadAllText("quotes.json"));
+
+            var r = new Random().Next(content.Quotes.Length);
+
+            Console.WriteLine(content.Quotes[r]);
+        }
+
+        public class Content
+        {
+            public string[] Quotes { get; set; }
+        }
+    }
+}
